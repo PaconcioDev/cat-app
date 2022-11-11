0931fe1475da6622ef25bb981b325ed71b6c39c5
Francisco Gomez <pgomezdev@gmail.com>
2022-11-11 10:43:19 -0500

    V_0.1.2 query parameters

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,4 @@
-const catApi = "https://api.thecatapi.com/v1/images/search";
+const catApi = "https://api.thecatapi.com/v1/images/search?limit=3";
 
 const btn = document.querySelector("button");
 btn.addEventListener("click", getCat);
@@ -7,8 +7,12 @@ async function getCat() {
   try {
     const res = await fetch(catApi);
     const data = await res.json();
-    const img = document.querySelector("img");
-    img.src = data[0].url;
+    const img1 = document.querySelector("#img1");
+    const img2 = document.querySelector("#img2");
+    const img3 = document.querySelector("#img3");
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
   } catch (error) {
     return new Error(error);
   }
